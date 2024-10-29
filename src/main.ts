903b2d536647d2d39d9487a3fed5385c89b57010
nicholas hung <nhung93@outlook.com>
2024-10-28 21:57:09 -0400

    working on a scene manager class to better manage the scene/make things configurable

--- src/main.ts
+++ src/main.ts
@@ -21,12 +21,12 @@ import { Blob as AnimatedBlob } from './visualizations/Blob';
 import { Spheres } from './visualizations/Spheres';
 import { Waves } from './visualizations/Waves';
 
-// important scene-related objects we might need to pass around
+/* important scene-related objects we might need to pass around
 interface ISceneComponents {
   renderer: WebGLRenderer, 
   scene: Scene, 
   camera: Camera
-}
+}*/
 
 // global variables
 let isPlaying = false;
@@ -52,6 +52,52 @@ const drawer = document.querySelector('.drawer');
 const showDrawer = document.getElementById('showDrawer');
 const hideDrawer = document.getElementById('hideDrawer');
 const bgColorPicker = document.getElementById('bgColorPicker');
+
+class SceneManager {
+  scene: Scene;
+  renderer: WebGLRenderer;
+  camera: Camera;
+  light: SpotLight;
+  
+  constructor(container: HTMLCanvasElement){
+    const scene = new Scene();
+    scene.background = new Color(0x111e37); //new Color(0xeeeeee);
+    this.scene = scene;
+    
+    const renderer = new WebGLRenderer({antialias: true});
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = PCFSoftShadowMap;
+    container.appendChild(renderer.domElement);
+    this.renderer = renderer;
+    
+    const fov = 60;
+    const camera = new PerspectiveCamera(
+      fov, 
+      container.clientWidth / container.clientHeight, 
+      0.01, 
+      1000
+    );
+      
+    camera.position.set(0, 2, 8);
+    scene.add(camera);
+    this.camera = camera;    
+    
+    // https://discourse.threejs.org/t/upgraded-to-latest-r160-and-my-lighting-has-changed/59879
+    const light = new SpotLight(); //0x34fcc5);
+    light.position.set(0, 20, 0);
+    light.castShadow = true;
+    light.shadow.mapSize.width = 1024;
+    light.shadow.mapSize.height = 1024;
+    light.intensity = 5;
+    light.distance = 0;
+    light.decay = 0; //0.1;
+    scene.add(light);
+    this.light = light;
+    
+    renderer.render(scene, camera);    
+  }
+}
 
 // stuff for canvas recording
 // helpful! https://devtails.xyz/@adam/how-to-record-html-canvas-using-mediarecorder-and-export-as-video
@@ -100,7 +146,7 @@ function stopCanvasRecord(){
   }
 }
 
-// TODO: maybe have a scene manager so we can more easily change lighting and stuff
+/* TODO: maybe have a scene manager so we can more easily change lighting and stuff
 function initializeScene(container: HTMLCanvasElement): ISceneComponents {
   const scene = new Scene();
   scene.background = new Color(0x111e37); //new Color(0xeeeeee);
@@ -136,7 +182,7 @@ function initializeScene(container: HTMLCanvasElement): ISceneComponents {
   renderer.render(scene, camera);
   
   return {renderer, scene, camera};
-}
+}*/
 
 function update(){
   renderer.render(scene, camera);
@@ -264,7 +310,12 @@ bgColorPicker?.addEventListener('change', (evt: Event) => {
 });
 
 // 3d stuff setup
-const { renderer, scene, camera } = initializeScene((canvasContainer as HTMLCanvasElement));
+const sceneManager = new SceneManager((canvasContainer as HTMLCanvasElement)); // initializes a scene
+const renderer = sceneManager.renderer;
+const scene = sceneManager.scene;
+const camera = sceneManager.camera;
+
+//const { renderer, scene, camera } = initializeScene((canvasContainer as HTMLCanvasElement));
 
 // stuff has loaded, hide loading message
 if(loadingMsg) loadingMsg.style.display = 'none';
