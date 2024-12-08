7ffa692463c4b32cd7194c0656bfb317e7326818
nicholas hung <nhung93@outlook.com>
2024-12-08 10:05:21 -0500

    updating sidebar UI a bit + added a Poisson disk sampling implementation attempt to experiment with star object placement for the starfield visualizer

--- src/visualizations/Starfield.ts
+++ src/visualizations/Starfield.ts
@@ -1,4 +1,7 @@
-import { VisualizerBase } from './VisualizerBase';
+import {
+  VisualizerBase,
+  ConfigurableParameterToggle
+} from './VisualizerBase';
 import { SceneManager } from '../SceneManager';
 import { AudioManager } from '../AudioManager';
 
@@ -44,9 +47,10 @@ export class Starfield extends VisualizerBase {
     this.loader = new GLTFLoader();
     this.xMin = xMin || -50;
     this.xMax = xMax || 50;
-    this.yMin = yMin || -20;
+    this.yMin = yMin || -50;
     this.yMax = yMax || 50;
     this.zMax = zMax || -100;
+    (this.configurableParams.bloomPass as ConfigurableParameterToggle).isOn = true;
   }
   
   loadModel(url: string){
@@ -70,36 +74,151 @@ export class Starfield extends VisualizerBase {
     return new Quaternion(x, y, z, w2);
   }
   
-  /* TODO: given a volume defined by width, height and length,
+  // given a volume defined by width, height and length,
   // generate a set of positions of length numDesiredPos that should be
   // distributed such that they are all at least minDist apart
+  //
+  // https://sighack.com/post/poisson-disk-sampling-bridsons-algorithm
+  // http://devmag.org.za/2009/05/03/poisson-disk-sampling/
+  // https://stackoverflow.com/questions/56749446/generalizion-3d-possion-disk-sampling
+  // also interesting: https://www.jasondavies.com/poisson-disc/
+  // and https://www.redblobgames.com/x/1830-jittered-grid/
   generatePoissonDiskSamplingSet(
-    width: number, 
-    height: number, 
-    length: number, 
-    minDist: number, 
+    width: number,
+    height: number,
+    length: number,
+    minDist: number,
     numDesiredPos: number
-  ): Array<Record<string, number>> {
-    // https://sighack.com/post/poisson-disk-sampling-bridsons-algorithm
-    // http://devmag.org.za/2009/05/03/poisson-disk-sampling/
-    // also interesting: https://www.jasondavies.com/poisson-disc/
-    
-    function getRandomCoord(width: number, height: number, length: number): Record<string, number>{
-      return {
-        x: Math.floor(Math.random() * width),
-        y: Math.floor(Math.random() * height),
-        z: Math.floor(Math.random() * length),
-      };
+  ): Array<Vector3> {
+    // 3d grid
+    // x dimension = width, y dimension = height, z dimension = length
+    function createGrid(): Array<Array<Array<Vector3 | null>>>{
+      const grid = [];
+      for(let x = 0; x < width; x++){
+        const xDim = [];
+        for(let y = 0; y < height; y++){
+          const yDim = [];
+          for(let z = 0; z < length; z++){
+            yDim.push(null);
+          }
+          xDim.push(yDim);
+        }
+        grid.push(xDim);
+      }
+      return grid;
     }
     
+    function checkNeighbors(
+      point: Vector3,
+      minDist: number,
+      cellSize: number,
+      grid: Array<Array<Array<Vector3 | null>>>
+    ): boolean{
+      if(
+        point.x < 0 ||
+        point.x >= width ||
+        point.y < 0 ||
+        point.y >= height ||
+        point.z < 0 ||
+        point.z >= length
+      ){
+        return false;
+      }
+      
+      // get the coordinates of point within the grid (i.e. which index in the grid does this point fall in)
+      const xIdx = Math.floor(point.x / cellSize);
+      const yIdx = Math.floor(point.y / cellSize);
+      const zIdx = Math.floor(point.z / cellSize);
+      
+      // check point's neighbors
+      for(let x = xIdx - 1; x <= xIdx + 1; x++){
+        for(let y = yIdx - 1; y <= yIdx + 1; y++){
+          for(let z = zIdx - 1; z <= zIdx + 1; z++){
+            // check distance between this cell's point, if any, and point
+            if(x < 0 || y < 0 || z < 0){
+              continue;
+            }
+            
+            if(xIdx != x && yIdx != y && zIdx != z && grid[x][y][z]){
+              const neighbor = grid[x][y][z];
+              if(neighbor && point.distanceTo(neighbor) < minDist){
+                return false;
+              }
+            }
+          }
+        }
+      }
+      
+      return true;
+    }
+    
+    function addToGrid(point: Vector3, cellSize:number, grid: Array<Array<Array<Vector3 | null>>>){
+      if(
+        point.x < 0 ||
+        point.x >= width ||
+        point.y < 0 ||
+        point.y >= height ||
+        point.z < 0 ||
+        point.z >= length
+      ){
+        // not sure this check is necessary but just in case
+        console.log('warn: point out of range, can\'t add to grid');
+        return;
+      }
+
+      // get the coordinates of point within the grid
+      const xIdx = Math.floor(point.x / cellSize);
+      const yIdx = Math.floor(point.y / cellSize);
+      const zIdx = Math.floor(point.z / cellSize);
+      grid[xIdx][yIdx][zIdx] = point;
+    }
+    
+    const grid = createGrid();
+    
     const cellSize = minDist / Math.sqrt(3); // 3 because we're in 3 dimensions
+    const result: Array<Vector3> = [];
+    let processing: Array<Vector3> = [new Vector3(0, 0, 0)];
     
-    const result: Array<Record<string, number>> = [];
-    
-    const processing = [getRandomCoord(width, height, length)];
+    while(processing.length > 0){
+      const curr = processing[Math.floor(Math.random() * processing.length)];
+      
+      let foundNewPos = false;
+      const tries = 25;
+      for(let i = 0; i < tries; i++){
+        // get a random point within minDist and 2*minDist of curr
+        // we can use Three.js for help with this via Vector3 :D
+        // https://stackoverflow.com/questions/74228946/how-to-get-a-random-vector3-within-a-sphere
+        const randomPoint = new Vector3();
+        randomPoint.randomDirection();
+        randomPoint.multiplyScalar(Math.random() * ((2 * minDist) - minDist) + minDist);
+        randomPoint.add(curr);
+        
+        // check against neighbors of random point to see if this random point
+        // is too close to an existing one.
+        const isOk = checkNeighbors(randomPoint, minDist, cellSize, grid);
+        
+        if(isOk){
+          processing.push(randomPoint);
+          addToGrid(randomPoint, cellSize, grid);
+          result.push(randomPoint);
+          foundNewPos = true;
+          break;
+        }
+      }
+      
+      if(!foundNewPos){
+        // remove the point from processing
+        processing = processing.filter(x => x !== curr);
+      }
+      
+      // add random point to result and processing list
+      if(result.length == numDesiredPos){
+        return result;
+      }
+    }
     
     return result;
-  }*/
+  }
   
   async init(){
     // clear the scene
@@ -122,15 +241,31 @@ export class Starfield extends VisualizerBase {
     starMaterial.emissiveIntensity = 0.8;
     
     const bufferLen = this.audioManager.analyser.frequencyBinCount;
-
-    const numObjects = this.numObjects;
     
     // if a small analyser.fftSize is chosen, frequencyBinCount will be small as well and
     // so Math.floor(bufferLen / numObjects) may end up being 0
-    const increment = Math.max(1, Math.floor(bufferLen / numObjects));
+    const increment = Math.max(1, bufferLen / this.numObjects);
     
+    /* TODO: need to experiment more with this and confirm it actually works per the algorithm
+    // at least with smaller minDist values, the stars get more clustered as expected so that's good I think
+    // maybe investigate more with adding a minDist wireframe sphere at each position to confirm
+    const positions = this.generatePoissonDiskSamplingSet(
+      Math.abs(this.xMax - this.xMin),
+      Math.abs(this.yMax - this.yMin),
+      Math.abs(this.zMax),
+      25,
+      this.numObjects + 1,
+    );*/
+    
+    //let posIdx = 0; // when using poisson disk sampling 
     for(let i = 0; i < bufferLen; i += increment){
       const star = starModel.clone();
+      
+      /* when using poisson disk sampling 
+      const xPos = positions[posIdx].x + this.xMin/3;
+      const yPos = positions[posIdx].y + this.yMin/2;
+      const zPos = positions[posIdx].z + this.zMax/3;
+      */
       
       const xPos = Math.floor(Math.random() * (this.xMax - this.xMin) + this.xMin);
       const yPos = Math.floor(Math.random() * (this.yMax - this.yMin) + this.yMin);
@@ -142,6 +277,8 @@ export class Starfield extends VisualizerBase {
       star.quaternion.copy(this.generateRandomQuaternion());
 
       this.visualization.add(star);
+      
+      //posIdx++;
     }
     
     this.scene.add(this.visualization);
