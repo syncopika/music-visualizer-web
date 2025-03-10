8175b0d40dfc67eff6a42eb04a046bf2d959f872
nicholas hung <nhung93@outlook.com>
2025-03-09 21:17:33 -0400

    cleanup some stuff, add params for positioning and scale for static image visualizer

--- src/visualizations/Image.ts
+++ src/visualizations/Image.ts
@@ -1,9 +1,12 @@
-import { VisualizerBase } from './VisualizerBase';
+import {
+  VisualizerBase,
+  ConfigurableParameterRange,
+  ConfigurableParameterToggle,
+} from './VisualizerBase';
 import { SceneManager } from '../SceneManager';
 import { AudioManager } from '../AudioManager';
 
-// TODO: allow user to import image to show on plane
-// check out https://github.com/syncopika/room-designer/blob/main/src/index.js
+// allow user to import image to show on plane
 // https://github.com/syncopika/room-designer/blob/main/src/index.js#L742
 // https://github.com/syncopika/room-designer/blob/main/src/index.js#L407
 
@@ -21,6 +24,16 @@ export class ImagePlane extends VisualizerBase {
   constructor(name: string, sceneManager: SceneManager, audioManager: AudioManager){
     super(name, sceneManager, audioManager);
     this.visualization = new Group();
+    
+    const params: Record<string, ConfigurableParameterRange> = {
+      'xPos': {value: 0, min: -15, max: 15, step: 0.5},
+      'yPos': {value: 1.5, min: -15, max: 15, step: 0.5},
+      'zPos': {value: -25, min: -50, max: 10, step: 0.5},
+      'xScale': {value: 1, min: 0.2, max: 3, step: 0.1},
+      'yScale': {value: 1, min: 0.2, max: 3, step: 0.1},
+    };
+    
+    Object.keys(params).forEach(p => this.configurableParams[p] = params[p]);
   }
   
   init(){
@@ -34,7 +47,7 @@ export class ImagePlane extends VisualizerBase {
       }
     });
     
-        // create plane mesh to hold image
+    // create plane mesh to hold image
     const planeGeometry = new PlaneGeometry(30, 30, 1, 1);
     const planeMaterial = new MeshBasicMaterial({color: 0xffffff, side: DoubleSide});
     const plane = new Mesh(planeGeometry, planeMaterial);
@@ -48,5 +61,14 @@ export class ImagePlane extends VisualizerBase {
   update(){
     this.doPostProcessing();
     //this.visualization.rotateY(Math.PI / 300);
+    
+    const params = this.configurableParams;
+    this.visualization.position.x = (params.xPos as ConfigurableParameterRange).value;
+    this.visualization.position.y = (params.yPos as ConfigurableParameterRange).value;
+    this.visualization.position.z = (params.zPos as ConfigurableParameterRange).value;
+    
+    const xScale = (params.xScale as ConfigurableParameterRange).value;
+    const yScale = (params.yScale as ConfigurableParameterRange).value;
+    this.visualization.scale.set(xScale, yScale, 1);
   }
 }
