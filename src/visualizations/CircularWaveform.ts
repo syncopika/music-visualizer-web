2a21cffb80087802df82676118fb6848a843a1f1
nicholas hung <nhung93@outlook.com>
2026-04-19 19:46:09 -0400

    working on another new waveform-based visualizer!

---
+++ src/visualizations/CircularWaveform.ts
@@ -0,0 +1,152 @@
+import { VisualizerBase } from './VisualizerBase';
+import { SceneManager } from '../SceneManager';
+import { AudioManager } from '../AudioManager';
+
+import {
+  Mesh,
+  BoxGeometry,
+  MeshPhongMaterial,
+  Vector3,
+  Group,
+} from 'three';
+
+export class CircularWaveform extends VisualizerBase {
+  numObjects: number;
+  visualization: Group;
+  lastTime: number;
+  moveTo: number[];
+  
+  constructor(name: string, sceneManager: SceneManager, audioManager: AudioManager, size: number){
+    super(name, sceneManager, audioManager);
+    this.numObjects = size;
+    this.visualization = new Group();
+    this.lastTime = this.clock.getElapsedTime();
+    this.moveTo = [];
+  }
+  
+  init(){
+    // clear the scene
+    this.scene.children.forEach(c => {
+      if(
+        !c.type.toLowerCase().includes('camera') && 
+        !c.type.toLowerCase().includes('light'))
+      {
+        this.scene.remove(c);
+      }
+    });
+    
+    const bufferLen = this.audioManager.analyser.frequencyBinCount;
+
+    const numObjects = this.numObjects; // TODO: not sure numObjects is accurate naming here?
+    
+    // arrange cubes in a circle
+    const radius = 15;
+    const angle = 360 / numObjects;
+    let currAngle = 0;
+    
+    // if a small analyser.fftSize is chosen, frequencyBinCount will be small as well and
+    // so Math.floor(bufferLen / numObjects) may end up being 0
+    const increment = Math.max(1, Math.floor(bufferLen / numObjects));
+    
+    const xIncrement = 0.93;
+    let xPos = -25;
+
+    // TODO: have sphere be an option also?
+    const createVisualizationCube = (): Mesh => {
+      const boxGeometry = new BoxGeometry(0.2, 0.2, 0.2);
+      const color = this.sceneManager.selectedColor ? this.sceneManager.selectedColor : '#aaff00';
+      const boxMaterial = new MeshPhongMaterial({color});
+      const box = new Mesh(boxGeometry, boxMaterial);
+      box.receiveShadow = true;
+      box.castShadow = true;
+      return box;
+    }
+    
+    for(let i = 0; i < bufferLen; i += increment){
+      const newCube = createVisualizationCube();
+
+      const rad = currAngle * (Math.PI / 180);
+      newCube.rotateY(-rad);
+      newCube.position.x = radius * Math.cos(rad);
+      newCube.position.z = radius * Math.sin(rad);
+      
+      currAngle += angle;
+      
+      this.visualization.add(newCube);
+
+      xPos += xIncrement;
+    }
+    
+    this.scene.add(this.visualization);
+    this.visualization.position.z = -15;
+    this.visualization.position.y -= 0.5;
+  }
+  
+  update(){
+    const bufferLength = this.audioManager.analyser.frequencyBinCount;
+    const buffer = this.audioManager.buffer;
+    const numObjects = this.visualization.children.length;
+    const increment = Math.floor(bufferLength / numObjects);
+    
+    this.audioManager.analyser.getByteTimeDomainData(buffer);
+    
+    const moveToIsEmpty = this.moveTo.length === 0;
+    const timeInterval = 0.04; // messing with this value can produce some interesting results!
+    const elapsedTime = this.clock.getElapsedTime();
+    const factor = 8;
+    
+    if(elapsedTime - this.lastTime >= timeInterval){
+      this.lastTime = elapsedTime;
+      
+      for(let i = 0; i < numObjects; i++){
+        const value = buffer[i * increment] / 255;
+        const y = value * factor;
+        //y = y < this.visualization.children[i].position.y ? 0 : y;
+
+        if(moveToIsEmpty){
+          this.moveTo.push(y);
+        }else{
+          this.moveTo[i] = y;
+        }
+      }
+    }else{
+      const lerpAmount = (elapsedTime - this.lastTime) / timeInterval;
+      
+      for(let i = 0; i < numObjects; i++){
+        const value = buffer[i * increment] / 255;
+        const y = value * factor;
+        //y = y < this.visualization.children[i].position.y ? 0 : y;
+        const obj = this.visualization.children[i];
+        
+        let valToMoveTo;
+        
+        if(moveToIsEmpty){
+          this.moveTo.push(y);
+          valToMoveTo = y;
+        }else{
+          valToMoveTo = this.moveTo[i];
+        }
+        
+        const newPos = new Vector3(obj.position.x, valToMoveTo, obj.position.z);
+      
+        obj.position.lerpVectors(
+          obj.position, 
+          newPos, 
+          lerpAmount,
+        );
+      }
+    }
+    
+    /*
+    for(let i = 0; i < numObjects; i++){
+      const value = buffer[i * increment] / 255; //128.0; // why 128?
+      const y = value * 5; // multiply by maximum height
+      const obj = this.visualization.children[i];
+      obj.position.lerp(new Vector3(obj.position.x, y, obj.position.z), 0.5);
+    }*/
+    
+    this.visualization.rotateY(Math.PI / 1000);
+    
+    this.doPostProcessing();
+  }
+}
