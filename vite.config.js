f91b4307b161e46ef2d74563a6d25b6c3e0e1efb
Nicholas H. <syncopika@users.noreply.github.com>
2026-06-18 12:31:17 -0400

    improve asset handling (#31)
    * improve asset handling by leveraging more vite features :D
    
    * fix up some things, e.g. specify gltf type and specify vite/client for compiler options in tsconfig, not vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // this base makes sure all my assets get the right path set because I'm sharing this app via GitHub Pages
+  base: '/music-visualizer-web/',
+  // https://github.com/vitejs/vite/issues/4953
+  assetsInclude: ['**/*.gltf'],
+});
